3e122600dac55ff68761761ff7ea7aacd6e6da22
Abby <i@abby.md>
2021-05-28 19:29:53 +0900

    1. Fix "PingFang SC" is not always exists 2. console content should contains all height 3. Make view more authentic 4. Can change to Vue.js homepage as background

--- js/panel.js
+++ js/panel.js
@@ -15,7 +15,7 @@
     Utils.extend(Moveable, Toggleable);
     Utils.extend(Panel, Moveable);
 
-    var prefix = '<div id="prefix" style="overflow:hidden;width:100%"><i class="fa fa-angle-right" aria-hidden="true" style="padding-right:5px"></i>';
+    var prefix = '<div id="prefix" style="overflow:hidden;width:100%"><i class="fa fa-angle-right" aria-hidden="true" style="padding-right:5px;color:#2162ac"></i>';
 
     Panel.prototype.append = function(str) {
         var split = str.split("\n");
